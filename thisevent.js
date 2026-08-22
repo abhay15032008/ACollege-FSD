12e0a1087fda1772f253dd4e92da62c1db2d1f25
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-08-22 21:27:51 +0530

    thisevents

---
+++ thisevent.js
@@ -0,0 +1,45 @@
+//⭐ this in event listener{
+// when 'this' is used in a callback of event handler of something , it refers to that something}
+
+// function changecolor(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor='blue';
+// }
+// let heading1=document.querySelector("h1");
+// heading1.addEventListener("click",changecolor);
+ 
+// let heading3=document.querySelector("h3");
+// heading3.addEventListener("click",changecolor)
+
+// let parag=document.querySelector("p");
+// parag.addEventListener("click",changecolor)
+
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",function(event){
+//     console.log(event);
+//     console.log("button clicked")
+// });
+// btn.addEventListener("dblclick",function(event){
+//     console.log(event);
+//     console.log("button clicked")
+// });
+
+
+//🏳️
+//⭐ Keyboard events
+// let inp=document.querySelector("input");
+// inp.addEventListener("keydown", function(){
+//     console.log("key was pressed")
+
+// })
+let inp=document.querySelector("input");
+inp.addEventListener("keydown", function(event){
+    console.log("code = ",event.code);
+if(event.code=="KeyU"){
+    console.log('character moves forward');
+    
+}else if(event.code=="KeyD"){
+    console.log("character moves backward");
+
+}
+});
